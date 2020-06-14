e845ba82b050227d780931ceac71ac918e66a4cd
Victor Batigin <bixs85@gmail.com>
2020-06-14 22:14:27 +0300

    added container and clean code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React from 'react';
 
-// import Profile from './components/Profile/Profile';
-// import user from './components/Profile/user.json';
+import Container from './components/Container/Container';
+
+import Profile from './components/Profile/Profile';
+import user from './components/Profile/user.json';
 
 import Statistics from './components/Statistics/Statistics';
 import statisticalData from './components/Statistics/statistical-data.json';
 
-// import FriendList from './components/Friend/FriendList/FriendList';
-// import friends from './components/Friend/FriendList/friends.json';
+import FriendList from './components/Friend/FriendList/FriendList';
+import friends from './components/Friend/FriendList/friends.json';
 
-// import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-// import transactions from './components/TransactionHistory/transactions.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
 
 const App = () => (
   <>
-    {/* <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    /> */}
+    <Container>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
 
-    <Statistics title="Upload files" stats={statisticalData} />
+      <Statistics title="Upload files" stats={statisticalData} />
 
-    {/* <FriendList friends={friends} /> */}
+      <FriendList friends={friends} />
 
-    {/* <TransactionHistory item={transactions} /> */}
+      <TransactionHistory item={transactions} />
+    </Container>
   </>
 );
 
